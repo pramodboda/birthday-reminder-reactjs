3b257354085adf8abd28d5d0c00588f98502c89a
pramodboda <pramodkumarboda@gmail.com>
2024-10-04 21:48:11 +0530

    UPDATE: Logic

--- src/components/BirthdayCard.tsx
+++ src/components/BirthdayCard.tsx
@@ -1,14 +1,35 @@
 import React, {useState} from "react";
 import BirthdayList from "./BirthdayList";
-import data, {Person} from "../data"
+import data, {Person} from "../data";
+
+// UI
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
 
 const BirthdayCard: React.FC = () => {
     const [people, setPeople] = useState<Person[]>(data);
     return (
     <main>
         <section className="card">
-            <h3>{people.length}</h3>
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        <CardDescription>
+
+                        </CardDescription>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                <h3>{people.length}</h3>
             <BirthdayList people={people}/>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full"  onClick={() => {setPeople([])}}>
+                    Clear All
+                    </Button>
+                </CardFooter>
+            </Card>
+          
             <button onClick={() => {setPeople([])}}>Clear All</button>
         </section>
     </main>)
