db1ca54fab7781519ba556fa1ebd972091c20811
pramodboda <pramodkumarboda@gmail.com>
2024-10-14 18:59:51 +0530

    UPDATE: Deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:"/birthday-reminder-reactjs/", // YOUR REPO NAME HERE
+  plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
